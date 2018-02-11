b1dee5bc4b9ded2afce83bd196011d163fe5d5d5
flicster <d.i.zykov@gmail.com>
2018-02-11 18:34:29 +0700

    Added: Command, Decorator, Strategy, Facade, Watcher, Sigleton

---
+++ src/com/facade/HomeTheaterTestDrive.java
@@ -0,0 +1,12 @@
+//package com.facade;
+//
+//public class HomeTheaterTestDrive {
+//    public static void main(String[] args) {
+//        //create sugjects components
+//        HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade(amp, tuner,
+//                                        dvdPlayer, cdPlayer, projector, lights, screen, popper);
+//        homeTheaterFacade.watchMovie("Raiders of the Lost Ark");
+//        homeTheaterFacade.endMovie();
+//        }
+//    }
+//}
